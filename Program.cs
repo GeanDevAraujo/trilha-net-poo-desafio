de18778d7c3c70437ce3ae496aa923e22b78706a
geanderson <geandersondearaujopassos@gmail.com>
2024-10-27 18:39:56 -0300

    Exercício Resolvido

--- Program.cs
+++ Program.cs
@@ -1,3 +1,16 @@
 ﻿using DesafioPOO.Models;
 
+//  public Nokia(string numero, string modelo, string imei, int memoria)
+
 // TODO: Realizar os testes com as classes Nokia e Iphone
+Smartphone nokia = new Nokia("2799642380", "J7", "272727", 126);
+nokia.InstalarAplicativo("WhatsApp");
+
+nokia.Ligar();
+nokia.ReceberLigacao();
+
+Smartphone iphone = new Iphone("121121212212", "7", "283736", 256);
+nokia.InstalarAplicativo("Twitter");
+
+nokia.Ligar();
+nokia.ReceberLigacao();
